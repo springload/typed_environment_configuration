baa0b7aa3256c60f66c0e90650af6f3ac90324b3
Peter Dekkers <425971+PeterDekkers@users.noreply.github.com>
2022-03-09 10:12:54 +1300

    Fix TypeConversionError reference, remove encoding declarations (not supporting Python 2 anymore), update version number

--- typed_environment_configuration/__init__.py
+++ typed_environment_configuration/__init__.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for Typed Environment Configuration."""
 
 __author__ = """Eugene Dementyev"""
 __email__ = "eugene@springload.co.nz"
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .typed_environment_configuration import *
